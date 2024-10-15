87d113ee77ab1c0de629339c52b7ac14b86128ae
iqbalhaidr <13523111@std.stei.itb.ac.id>
2024-10-15 12:48:08 +0700

    feat: kekacauan.c

---
+++ if2110-algoritmastrukturdata/p04-matrix/kekacauan.c
@@ -0,0 +1,35 @@
+#include "matrix.h"
+#include <stdio.h>
+
+int main() {
+    Matrix papan;
+    int n;
+    int kekacauan = 0;
+
+    scanf("%d", &n);
+    readMatrix(&papan, n, n);
+
+    int kekacauanRow[n], kekacauanCol[n];
+    for (int i=0; i<n; ++i) {
+        kekacauanRow[i] = 0;
+        kekacauanCol[i] = 0;
+    }
+
+    for (int i=0; i<=getLastIdxRow(papan); ++i) {
+        for (int j=0; j<=getLastIdxCol(papan); ++j) {
+            kekacauanRow[i] += ELMT(papan, i, j);
+            kekacauanCol[j] += ELMT(papan, i, j);
+        }
+    }
+
+    for (int i=0; i<=getLastIdxRow(papan); ++i) {
+        for (int j=0; j<=getLastIdxCol(papan); ++j) {
+            if (ELMT(papan, i, j) > 0) {
+                kekacauan += kekacauanRow[i] + kekacauanCol[j] - 2*ELMT(papan, i, j);
+            }
+        }
+    }
+
+    printf("%d\n", kekacauan);
+    return 0;
+}
