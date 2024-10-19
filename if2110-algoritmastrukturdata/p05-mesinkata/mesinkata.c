bd81a2056b91dedacc423c8dc585787108010098
iqbalhaidr <13523111@std.stei.itb.ac.id>
2024-10-19 16:35:05 +0700

    feat: mesinkarakter.c & mesinkata.c

---
+++ if2110-algoritmastrukturdata/p05-mesinkata/mesinkata.c
@@ -0,0 +1,43 @@
+#include "mesinkata.h"
+
+boolean EndWord;
+Word currentWord;
+
+void IgnoreBlanks() {
+   while (currentChar == BLANK) {
+      ADV();
+   }
+}
+
+void STARTWORD() {
+   START();
+   IgnoreBlanks();
+   if (currentChar == MARK) {
+      EndWord = true;
+   } else {
+      EndWord = false;
+      CopyWord();
+   }
+}
+
+void ADVWORD() {
+   IgnoreBlanks();
+   if (currentChar == MARK) {
+      EndWord = true;
+   } else {
+      CopyWord();
+      IgnoreBlanks();
+   }
+}
+
+void CopyWord() {
+   int i = 0;
+   while (currentChar!=MARK && currentChar!=BLANK) {
+      if (i<NMax) {
+         currentWord.TabWord[i] = currentChar;
+         ++i;
+      }
+      ADV();
+   }
+   currentWord.Length = i;
+}
